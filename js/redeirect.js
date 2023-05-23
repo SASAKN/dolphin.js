c1149ae7f13135473ddaf085bd6e37e12c9d9fe0
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-05-23 19:51:52 +0900

    Update redeirect.js

--- js/redeirect.js
+++ js/redeirect.js
@@ -1,18 +1,1 @@
-// iPhone
-if ( navigator.userAgent.indexOf('iPhone') > 0 ){
-    location.href = 'sumaho/index.html';
-    // iPad
-    } else if( navigator.userAgent.indexOf('iPad') > 0 ){
-        location.href = 'sumaho/index.html';
-    // iPod
-    } else if( navigator.userAgent.indexOf('iPod') > 0 ){
-        location.href = 'sumaho/index.html';
-    // Android
-    } else if( navigator.userAgent.indexOf('Android') > 0 ){
-        document.write('<title>iPhoneに機種変更してください。</title><h1>Androidは、非対応です。</h1>');
-     function iphokae() {
-         location.href = 'https://www.apple.com/jp/iphone/';
-     }
-   
-     setTimeout(iphokae,5000);
-    }
+
