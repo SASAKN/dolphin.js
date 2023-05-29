ceeecc33262069f2cdbea80550f763ad45bfa269
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-05-29 18:17:36 +0900

    Update

--- js/conmenu.js
+++ js/conmenu.js
@@ -1,8 +1,15 @@
 window.onload = function () {
     let con = document.getElementById('conmenu');  
     let rightclick = document.getElementById('rightclick');     
-    let body = document.body;                       
-    rightclick.addEventListener('click', function (e) {
+    let body = document.body;
+    if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+      rightclick.addEventListener('click', function () {
+        con.style.left = '183px';
+        con.style.top = '100px';
+        con.classList.add('show');
+      });
+    }                       
+    rightclick.addEventListener('contextmenu', function (e) {
       con.style.left = e.pageX + 'px';
       con.style.top = e.pageY + 'px';
       con.classList.add('show');
