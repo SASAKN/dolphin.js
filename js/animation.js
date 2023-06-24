2806d4718ec5a2846346324e317ce51d6b806430
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-06-24 14:49:30 +0900

    リファクタリング中

--- js/animation.js
+++ js/animation.js
@@ -1,234 +1,65 @@
 function anime() {
-  document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/1/1.gif" class="anim1">';
+  var animations = [
+    { gif: 'anime/1/1.gif', audio: 'anime/1/1.mp3', duration: 5000 },
+    { gif: 'anime/2/2.gif', audio: 'anime/2/2.mp3', duration: 28000 },
+    { gif: 'anime/3/3.gif', audio: 'anime/3/3.mp3', duration: 4000 },
+    { gif: 'anime/4/4.gif', audio: 'anime/4/4.mp3', duration: 7000 },
+    { gif: 'anime/5/5.gif', audio: 'anime/5/5.mp3', duration: 5000 },
+    { gif: 'anime/6/6.gif', audio: 'anime/6/6.mp3', duration: 3000 },
+    { gif: 'anime/7/7.gif', audio: 'anime/7/7.mp3', duration: 3000 },
+    { gif: 'anime/8/8.gif', audio: 'anime/8/8.mp3', duration: 2000 },
+    { gif: 'anime/9/9.gif', audio: 'anime/9/9.mp3', duration: 2000 },
+    { gif: 'anime/10/10.gif', audio: 'anime/10/10.mp3', duration: 22000 },
+    { gif: 'anime/11/11.gif', audio: 'anime/11/11.mp3', duration: 5000 },
+    { gif: 'anime/12/12.gif', audio: 'anime/12/12.mp3', duration: 20000 },
+    { gif: 'anime/13/13.gif', audio: 'anime/13/13.mp3', duration: 3000 },
+    { gif: 'anime/14/14.gif', audio: 'anime/14/14.mp3', duration: 3000 },
+    { gif: 'anime/15/15.gif', audio: 'anime/15/15.mp3', duration: 3000 },
+    { gif: 'anime/16/16si.gif', audio: 'anime/16/16.mp3', duration: 15000 },
+    { gif: 'anime/17/17.gif', audio: 'anime/17/17.mp3', duration: 3000 },
+    { gif: 'anime/18/18.gif', audio: 'anime/18/18.mp3', duration: 5000 },
+    { gif: 'anime/19/19.gif', audio: 'anime/19/19.mp3', duration: 5000 },
+    { gif: 'anime/20/20.gif', audio: 'anime/20/20.mp3', duration: 5000 },
+    { gif: 'anime/21/21.gif', audio: 'anime/21/21.mp3', duration: 5000 },
+    { gif: 'anime/22/22.gif', audio: 'anime/22/22.mp3', duration: 5000 },
+    { gif: 'anime/23/23.gif', audio: 'anime/23/23.mp3', duration: 5000 },
+    { gif: 'anime/24/24.gif', audio: 'anime/24/24.mp3', duration: 5000 },
+    { gif: 'anime/25/25.gif', audio: 'anime/25/25.mp3', duration: 5000 },
+    { gif: 'anime/26/26.gif', audio: 'anime/26/26.mp3', duration: 9000 },
+    { gif: 'anime/27/27.gif', audio: 'anime/27/27.mp3', duration: 11000 },
+    { gif: 'anime/28/28.gif', audio: 'anime/28/28.mp3', duration: 5000 },
+    { gif: 'anime/29/29.gif', audio: 'anime/29/29.mp3', duration: 30000 },
+    { gif: 'anime/30/30si.gif', audio: 'anime/30/30.mp3', duration: 10000 },
+    { gif: 'anime/31/31.gif', audio: 'anime/31/31.mp3', duration: 30000 },
+    { gif: 'anime/32/32.gif', audio: 'anime/32/32.mp3', duration: 2000 },
+    { gif: 'anime/33/33.gif', audio: 'anime/33/33.mp3', duration: 35000 }
+  ];
 
-  var play2 = new Audio('anime/1/1.mp3');
-  play2.play();
-  setTimeout(anim2, 5000);
-  function anim2() {
-    document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/2/2.gif" class="anim2">';
+  var currentIndex = 0;
+  var kyleImage = document.getElementById('kyleimagevideo');
 
-    var play2 = new Audio('anime/2/2.mp3');
-    play2.play();
-    setTimeout(anim3, 28000);
-    function anim3() {
-      document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/3/3.gif" class="anim3">';
-
-      var play2 = new Audio('anime/3/3.mp3');
+  function playAnimation() {
+    if (currentIndex >= animations.length) {
+      kyleImage.innerHTML = '<img src="anime/33/33.gif" class="anim12">';
+      var play2 = new Audio('anime/33/33.mp3');
       play2.play();
-      setTimeout(anim4, 4000);
-      function anim4() {
-        document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/4/4.gif" class="anim4">';
-
-        var play2 = new Audio('anime/4/4.mp3');
-        play2.play();
-        setTimeout(anim5, 7000);
-        function anim5() {
-          document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/5/5.gif" class="anim5">';
-
-          var play2 = new Audio('anime/5/5.mp3');
-          play2.play();
-          setTimeout(anim6, 5000);
-          function anim6() {
-            document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/6/6.gif" class="anim6">';
-
-            var play2 = new Audio('anime/6/6.mp3');
-            play2.play();
-            setTimeout(anim7, 3000);
-            function anim7() {
-              document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/7/7.gif" class="anim7">';
-
-              var play2 = new Audio('anime/7/7.mp3');
-              play2.play();
-              setTimeout(anim8, 3000);
-              function anim8() {
-                document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/8/8.gif" class="anim8">';
-
-                var play2 = new Audio('anime/8/8.mp3');
-                play2.play();
-                setTimeout(anim9, 2000);
-                function anim9() {
-                  document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/9/9.gif" class="anim9">';
-
-                  var play2 = new Audio('anime/9/9.mp3');
-                  play2.play();
-                  setTimeout(anim10, 2000);
-                  function anim10() {
-                    document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/10/10.gif" class="anim10">';
-
-                    var play2 = new Audio('anime/10/10.mp3');
-                    play2.play();
-                    setTimeout(anim11, 22000);
-                    function anim11() {
-                      document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/11/11.gif" class="anim11">';
-
-                      var play2 = new Audio('anime/11/11.mp3');
-                      play2.play();
-                      setTimeout(anim12, 5000);
-                      function anim12() {
-                        document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/12/12.gif" class="anim12">';
-
-                        var play2 = new Audio('anime/12/12.mp3');
-                        play2.play();
-                        setTimeout(anim13, 20000);
-                        function anim13() {
-                          document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/13/13.gif" class="anim13">';
-
-                          var play2 = new Audio('anime/13/13.mp3');
-                          play2.play();
-                          setTimeout(anim14, 3000);
-                          function anim14() {
-                            document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/14/14.gif" class="anim14">';
-
-                            var play2 = new Audio('anime/14/14.mp3');
-                            play2.play();
-                            setTimeout(anim15, 3000);
-                            function anim15() {
-                              document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/15/15.gif" class="anim15">';
-
-                              var play2 = new Audio('anime/15/15.mp3');
-                              play2.play();
-                              setTimeout(anim16, 3000);
-                              function anim16() {
-                                document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/16/16si.gif" class="anim16">';
-
-                                var play2 = new Audio('anime/16/16.mp3');
-                                play2.play();
-                                setTimeout(anim17, 15000);
-                                function anim17() {
-                                  document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/17/17.gif" class="anim17">';
-
-                                  var play2 = new Audio('anime/17/17.mp3');
-                                  play2.play();
-                                  setTimeout(anim18, 5000);
-                                  function anim18() {
-                                    document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/18/18.gif" class="anim18">';
-
-                                    var play2 = new Audio('anime/18/18.mp3');
-                                    play2.play();
-                                    setTimeout(anim19, 5000);
-                                    function anim19() {
-                                      document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/19/19.gif" class="anim19">';
-
-                                      var play2 = new Audio('anime/19/19.mp3');
-                                      play2.play();
-                                      setTimeout(anim20, 5000);
-                                      function anim20() {
-                                        document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/20/20.gif" class="anim20">';
-
-                                        var play2 = new Audio('anime/20/20.mp3');
-                                        play2.play();
-                                        setTimeout(anim21, 5000);
-                                        function anim21() {
-                                          document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/21/21.gif" class="anim21">';
-
-                                          var play2 = new Audio('anime/21/21.mp3');
-                                          play2.play();
-                                          setTimeout(anim22, 5000);
-                                          function anim22() {
-                                            document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/22/22.gif" class="anim22">';
-
-                                            var play2 = new Audio('anime/22/22.mp3');
-                                            play2.play();
-                                            setTimeout(anim23, 5000);
-                                            function anim23() {
-                                              document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/23/23.gif" class="anim23">';
-
-                                              var play2 = new Audio('anime/23/23.mp3');
-                                              play2.play();
-                                              setTimeout(anim24, 5000);
-                                              function anim24() {
-                                                document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/24/24.gif" class="anim24">';
-
-                                                var play2 = new Audio('anime/24/24.mp3');
-                                                play2.play();
-                                                setTimeout(anim25, 5000);
-                                                function anim25() {
-                                                  document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/25/25.gif" class="anim25">';
-
-                                                  var play2 = new Audio('anime/25/25.mp3');
-                                                  play2.play();
-                                                  setTimeout(anim26, 9000);
-                                                  function anim26() {
-                                                    document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/26/26.gif" class="anim26">';
-
-                                                    var play2 = new Audio('anime/26/26.mp3');
-                                                    play2.play();
-                                                    setTimeout(anim27, 11000);
-                                                    function anim27() {
-                                                      document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/27/27.gif" class="anim27">';
-
-                                                      var play2 = new Audio('anime/27/27.mp3');
-                                                      play2.play();
-                                                      setTimeout(anim28, 5000);
-                                                      function anim28() {
-                                                        document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/28/28.gif" class="anim28">';
-
-                                                        var play2 = new Audio('anime/28/28.mp3');
-                                                        play2.play();
-                                                        setTimeout(anim29, 30000);
-                                                        function anim29() {
-                                                          document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/29/29.gif" class="anim29">';
-
-                                                          var play2 = new Audio('anime/29/29.mp3');
-                                                          play2.play();
-                                                          setTimeout(anim30, 10000);
-                                                          function anim30() {
-                                                            document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/30/30si.gif" class="anim30">';
-
-                                                            var play2 = new Audio('anime/30/30.mp3');
-                                                            play2.play();
-                                                            setTimeout(anim31, 30000);
-                                                            function anim31() {
-                                                              document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/31/31.gif" class="anim31">';
-
-                                                              var play2 = new Audio('anime/31/31.mp3');
-                                                              play2.play();
-                                                              setTimeout(anim32, 2000);
-                                                              function anim32() {
-                                                                document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/32/32.gif" class="anim32">';
-
-                                                                var play2 = new Audio('anime/32/32.mp3');
-                                                                play2.play();
-                                                                setTimeout(finalanim, 5000);
-                                                                function finalanim() {
-                                                                  document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/33/33.gif" class="anim12">';
-
-                                                                  var play2 = new Audio('anime/33/33.mp3');
-                                                                  play2.play();
-                                                                  setTimeout(finanim, 35000);
-                                                                  function finanim() {
-                                                                    document.getElementById('kyleimagevideo').innerHTML = '          <video muted control="false" autoplay loop class="image_kyle animk" id="rightclick">            <source src="./dolphin.webm" type="video/webm" />          </video>';
-                                                                  }
-                                                                }
-                                                              }
-                                                            }
-                                                          }
-                                                        }
-                                                      }
-                                                    }
-                                                  }
-                                                }
-                                              }
-                                            }
-                                          }
-                                        }
-                                      }
-                                    }
-                                  }
-                                }
-                              }
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+      setTimeout(finalAnim, 35000);
+      return;
     }
+
+    var animation = animations[currentIndex];
+    kyleImage.innerHTML = '<img src="' + animation.gif + '" class="anim' + (currentIndex + 1) + '">';
+
+    var audio = new Audio(animation.audio);
+    audio.play();
+
+    setTimeout(playNextAnimation, animation.duration);
   }
+
+  function playNextAnimation() {
+    currentIndex++;
+    playAnimation();
+  }
+
+  playAnimation();
 }
