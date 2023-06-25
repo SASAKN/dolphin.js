e8e4a829b7588b48036aef8cff05b132cf70b1e2
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-06-25 16:09:46 +0900

    仕掛けが書かれているスクリプトは、統合されました。

--- js/sikake.js
+++
@@ -1,8 +0,0 @@
-function katakata() {
-    document.getElementById('kyleimagevideo').innerHTML = '<img src="anime/33/33.gif" class="anim33">';
-  
-    var play2 = new Audio('anime/33/33.mp3');
-  
-    play2.play();
-    setTimeout('play2.pause', 35000);
-  }
