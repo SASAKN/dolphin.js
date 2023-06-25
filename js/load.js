78db2d9de33cb1f053fa09e7ea54c52cdb14a6e5
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-06-25 15:03:01 +0900

    ロード画面もリファクタリング

--- js/load.js
+++ js/load.js
@@ -1,9 +1,4 @@
-function fini() {
-
-        
+setTimeout(function(){
         const spinner = document.getElementById('screen');
         spinner.classList.add('loaded');
-        var a = new Audio('../start.wav');
-        a.play();
-}
-setTimeout(fini,5000);
+}, 5000);
