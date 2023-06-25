647ed2ab0428e89a45e8aa8e74068c26f78d76f4
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-06-25 15:14:23 +0900

    jQueryを使用しないでも音声認識ができるように

--- js/onsei.js
+++ js/onsei.js
@@ -1,13 +1,11 @@
-window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 var recognition = new webkitSpeechRecognition();
 recognition.lang = 'ja';
 
 recognition.addEventListener('result', function(event) {
     var text = event.results.item(0).item(0).transcript;
-    $("#recordtext").val(text);
+    document.getElementById('recordtext').value = text;
 }, false);
 
-//開始
 function record() {
     recognition.start();
 }
